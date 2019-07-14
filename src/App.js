f5f0ac1cc89b232066de7cccd66a3954e286ac68
Luciano de Souza Nery <lsnery@gmail.com>
2019-07-13 22:06:54 -0300

    adicionado styled component global

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
 import Routes from './routes';
+import GlogalStyle from './styles/global';
 
 function App() {
-	return <Routes />;
+	return (
+		<>
+			<Routes />
+			<GlogalStyle />
+		</>
+	);
 }
 
 export default App;
